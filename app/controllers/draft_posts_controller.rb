4839fe03af21ad57c2c9a64d75a96ee93d422050
Robert Lee Brackett <robert.brackett3@gmail.com>
2011-07-19 15:26:00 -0400

    initial styles for draft post index

--- app/controllers/draft_posts_controller.rb
+++ app/controllers/draft_posts_controller.rb
@@ -1,4 +1,5 @@
 class DraftPostsController < ApplicationController
+  before_filter  :admin_login_required
   # GET /draft_posts
   # GET /draft_posts.json
   def index
@@ -18,18 +19,6 @@ class DraftPostsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @draft_post }
-    end
-  end
-
-  # DELETE /draft_posts/1
-  # DELETE /draft_posts/1.json
-  def destroy
-    @draft_post = Post.find(params[:id])
-    @draft_post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to draft_posts_url }
-      format.json { head :ok }
     end
   end
 
--- test/functional/draft_posts_controller_test.rb
+++ test/functional/draft_posts_controller_test.rb
@@ -5,13 +5,27 @@ class DraftPostsControllerTest < ActionController::TestCase
     @draft_post = posts(:draft)
   end
 
-  test "should get index" do
+# index action, get
+  test "index: should restrict access" do
+    get :index
+    assert_redirected_to login_path
+  end
+
+  test "index: should get index" do
+    login_admin
     get :index
     assert_response :success
     assert_not_nil assigns(:draft_posts)
   end
 
-  test "should show draft_post" do
+#show action, get
+  test "show: should restrict access" do
+    get :show, id: 1
+    assert_redirected_to login_path
+  end
+
+  test "show: should show draft_post" do
+    login_admin
     get :show, id: @draft_post.to_param
     assert_response :success
   end
